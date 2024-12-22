b4e5828e67ca54a5a7ee050123677dbfba8aa985
xBartM <mielczarek.cs@gmail.com>
2024-12-22 18:34:02 +0000

    added new columns to the schema

--- database/create_schema.sql
+++ database/create_schema.sql
@@ -22,7 +22,10 @@ CREATE TABLE ytkp.video (
     channel_id INT REFERENCES ytkp.channel(channel_id),
     video_url VARCHAR(255) NOT NULL,
     video_name VARCHAR(255),
-    video_description TEXT
+    video_description TEXT,
+    upload_date DATE NOT NULL,
+    insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    scrape_date TIMESTAMP DEFAULT '1970-01-01 00:00:00' NOT NULL
 );
 
 -- Add comments for the video table and its columns
@@ -32,6 +35,9 @@ COMMENT ON COLUMN ytkp.video.channel_id IS 'Foreign key referencing the associat
 COMMENT ON COLUMN ytkp.video.video_url IS 'URL of the video, must be provided';
 COMMENT ON COLUMN ytkp.video.video_name IS 'Name of the video';
 COMMENT ON COLUMN ytkp.video.video_description IS 'Description of the video';
+COMMENT ON COLUMN ytkp.video.upload_date IS 'Original upload date of the video';
+COMMENT ON COLUMN ytkp.video.insert_date IS 'Timestamp of row insertion';
+COMMENT ON COLUMN ytkp.video.scrape_date IS 'Timestamp of the last time the video was scraped for data';
 
 
 -- Create the comment table
@@ -40,7 +46,9 @@ CREATE TABLE ytkp.comment_section (
     video_id INT REFERENCES ytkp.video(video_id),
    -- channel_id INT REFERENCES ytkp.channel(channel_id),
     user_name VARCHAR(255),
-    comment_text TEXT
+    comment_text TEXT,
+    comment_date TIMESTAMP NOT NULL,
+    insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 -- Add comments for the comment table and its columns
@@ -49,3 +57,5 @@ COMMENT ON COLUMN ytkp.comment_section.comment_id IS 'Unique identifier for each
 COMMENT ON COLUMN ytkp.comment_section.video_id IS 'Foreign key referencing the associated video';
 COMMENT ON COLUMN ytkp.comment_section.user_name IS 'Name of the user that wrote the given comment';
 COMMENT ON COLUMN ytkp.comment_section.comment_text IS 'Text of the comment';
+COMMENT ON COLUMN ytkp.comment_section.comment_date IS 'Original submit date of the comment to YT service';
+COMMENT ON COLUMN ytkp.comment_section.insert_date IS 'Date of insertion of the comment to the database';
