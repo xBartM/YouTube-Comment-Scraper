1df68ee8528b0c07fb0665d5538d1a49c6fb897c
xBartM <mielczarek.cs@gmail.com>
2024-12-27 13:04:32 +0000

    redirected pg_ctl output to database/logs

--- scripts/db_start.sh
+++ scripts/db_start.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+# check if YTKP_DIR is set
+if [ -z "${YTKP_DIR}" ] ; then
+    echo "YTKP_DIR is not set."
+    exit 1
+fi
+
 # run everytime the shell is restarted to start postgres process
-pg_ctl -D $PREFIX/var/lib/postgresql start
+pg_ctl \
+ --pgdata=$PREFIX/var/lib/postgresql \
+ --log=${YTKP_DIR}/database/logs/pg_ctl.log \
+ start
--- scripts/db_stop.sh
+++ scripts/db_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 # run everytime the shell is restarted to start postgres process
-pg_ctl -D $PREFIX/var/lib/postgresql stop
+pg_ctl \
+ --pgdata=$PREFIX/var/lib/postgresql \
+ --mode=smart \
+ stop
